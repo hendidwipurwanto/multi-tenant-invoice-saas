1e547fd76b03b416662a92d9956c3a18dbaebb99
hendi.dp <hendi.dp@pttmg.com>
2026-03-24 09:25:27 +0700

    Added Invoice and InvoiceItem Entities

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Entities
+{
+    public class Invoice : BaseEntity
+    {
+        public string InvoiceNumber { get; set; }
+
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public decimal Subtotal { get; private set; }
+        public decimal Tax { get; private set; }
+        public decimal Total { get; private set; }
+
+        public InvoiceStatus Status { get; private set; }
+
+        public ICollection<InvoiceItem> Items { get; set; } = new List<InvoiceItem>();
+
+        public void CalculateTotals()
+        {
+            Subtotal = Items.Sum(x => x.Quantity * x.UnitPrice);
+            Tax = Subtotal * 0.11m; // example
+            Total = Subtotal + Tax;
+        }
+    }
+}
---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/InvoiceItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Entities
+{
+    public class InvoiceItem
+    {
+        public Guid Id { get; set; }
+
+        public Guid InvoiceId { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
