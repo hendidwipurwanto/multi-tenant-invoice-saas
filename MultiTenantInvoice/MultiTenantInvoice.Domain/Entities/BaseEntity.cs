4af08ffdc2d5dd69f912f15e4ec660d2adf0d58e
hendi.dp <hendi.dp@pttmg.com>
2026-03-24 18:49:30 +0700

    updated  based entity property, added Invoice properties and added IDomainEvent

--- MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/BaseEntity.cs
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/BaseEntity.cs
@@ -9,12 +9,15 @@ namespace MultiTenantInvoice.Domain.Entities
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
+
         public Guid TenantId { get; set; }
 
         public DateTime CreatedAt { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
 
         public DateTime? UpdatedAt { get; set; }
         public string? UpdatedBy { get; set; }
+
+        public bool IsDeleted { get; set; }
     }
 }
