2d5e0d28b3c5d33488f679ea8063e84284d2cc20
hendi.dp <hendi.dp@pttmg.com>
2026-03-24 18:59:25 +0700

    Configure Customers, Invoices andTenants  configuration

---
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Persistence/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTenantInvoice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Infrastructure.Persistence.Configurations
+{
+    public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.InvoiceNumber)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Subtotal)
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(x => x.Total)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasMany(x => x.Items)
+                .WithOne()
+                .HasForeignKey(x => x.InvoiceId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Persistence/Configurations/TenantConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTenantInvoice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Infrastructure.Persistence.Configurations
+{
+    public class TenantConfiguration : IEntityTypeConfiguration<Tenant>
+    {
+        public void Configure(EntityTypeBuilder<Tenant> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Subdomain)
+                .HasMaxLength(100);
+        }
+    }
+}
