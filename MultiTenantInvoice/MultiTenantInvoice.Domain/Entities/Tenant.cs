4e477ea9e36f7894c9cea512b5f55e3317f0cf91
hendi.dp <hendi.dp@pttmg.com>
2026-03-24 09:26:03 +0700

    Added Tenant and User Entities

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/Tenant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Entities
+{
+    public class Tenant : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Subdomain { get; set; } // future SaaS routing
+    }
+}
