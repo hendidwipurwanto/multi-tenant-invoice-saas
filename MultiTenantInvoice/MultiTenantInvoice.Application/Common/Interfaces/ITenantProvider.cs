de748f93f7461be8a7df46bbbaa67779662f7070
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 07:24:29 +0700

    Added TenantProvider

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Common/Interfaces/ITenantProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Common.Interfaces
+{
+    public interface ITenantProvider
+    {
+        Guid GetTenantId();
+    }
+}
---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Common/Tenant/TenantContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Common.Tenant
+{
+    public class TenantContext
+    {
+        public Guid TenantId { get; set; }
+    }
+}
