7b2e6c29836c03afc50c60519856ad152ba7b1e5
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 16:36:00 +0700

    Added CreateInvoiceItemDto

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Invoices/DTOs/CreateInvoiceItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Invoices.DTOs
+{
+    public class CreateInvoiceItemDto
+    {
+        public string Description { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}
