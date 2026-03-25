2195e095077b497b115bc250bf16fe24b9ba51ff
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 08:02:15 +0700

    added interface  IMultiTenantEntity and ISoftDelete

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Common/IMultiTenantEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Common
+{
+    public interface IMultiTenantEntity
+    {
+        Guid TenantId { get; set; }
+    }
+}
