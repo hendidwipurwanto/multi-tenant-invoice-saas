516cc570243e16adb2a71266e860a044ef9ae0b4
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 16:38:33 +0700

    added CreateInvoiceCommand.cs and added  CreateInvoiceRequest.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Invoices/Commands/CreateInvoiceCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MultiTenantInvoice.Application.Features.Invoices.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Invoices.Commands
+{
+    public class CreateInvoiceCommand : IRequest<Guid>
+    {
+        public CreateInvoiceRequest Request { get; set; }
+    }
+}
---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Invoices/DTOs/CreateInvoiceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Invoices.DTOs
+{
+    public class CreateInvoiceRequest
+    {
+        public Guid CustomerId { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public List<CreateInvoiceItemDto> Items { get; set; }
+    }
+}
