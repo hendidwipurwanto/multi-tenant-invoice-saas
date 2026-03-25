c48329b61b685dfc485bc9b3bee27978044f6ca1
hendi.dp <hendi.dp@pttmg.com>
2026-03-26 06:22:37 +0700

    Added InvoiceNumberSequences table

---
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Migrations/20260325231055_Add_InvoiceNumberSequence_table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiTenantInvoice.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_InvoiceNumberSequence_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InvoiceNumberSequences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    LastNumber = table.Column<int>(type: "int", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceNumberSequences", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InvoiceNumberSequences");
+        }
+    }
+}
--- MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Persistence/AppDbContext.cs
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Persistence/AppDbContext.cs
@@ -27,6 +27,8 @@ namespace MultiTenantInvoice.Infrastructure.Persistence
 
         public DbSet<Invoice> Invoices { get; set; }
 
+        public DbSet<InvoiceNumberSequence> InvoiceNumberSequences { get; set; }
+
         public DbSet<InvoiceItem> InvoiceItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
