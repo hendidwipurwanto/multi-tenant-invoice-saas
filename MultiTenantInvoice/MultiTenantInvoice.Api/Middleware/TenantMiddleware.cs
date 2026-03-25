3e9a78d510014ece80af211b41f55a05a1218a1e
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 07:21:42 +0700

    Added TenantMiddleware

---
+++ MultiTenantInvoice/MultiTenantInvoice.Api/Middleware/TenantMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using MultiTenantInvoice.Application.Common.Tenant;
+
+namespace MultiTenantInvoice.Api.Middleware
+{
+    public class TenantMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TenantMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TenantContext tenantContext)
+        {
+            if (context.Request.Headers.TryGetValue("X-Tenant-Id", out var tenantId))
+            {
+                if (Guid.TryParse(tenantId, out var parsedTenant))
+                {
+                    tenantContext.TenantId = parsedTenant;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
