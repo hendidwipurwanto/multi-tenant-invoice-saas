d901596cc4e538e5596068e60367259ae6d765a7
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 07:52:14 +0700

    Added  BaseEntity implement IMultiTenantEntity and ISoftDelete

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Common
+{
+    public abstract class BaseEntity : IMultiTenantEntity, ISoftDelete
+    {
+        public Guid Id { get; set; }
+
+        public Guid TenantId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
