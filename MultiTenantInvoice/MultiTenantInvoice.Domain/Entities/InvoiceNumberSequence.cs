ec0c0b7adf9bf7d2c1d73600c6ce9e24b073fd92
hendi.dp <hendi.dp@pttmg.com>
2026-03-25 16:41:41 +0700

    Added InvoiceNumberSequence.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Entities/InvoiceNumberSequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Entities
+{
+    public class InvoiceNumberSequence : BaseEntity
+    {
+        public int Year { get; set; }
+
+        public int LastNumber { get; set; }
+
+    }
+}
