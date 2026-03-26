1419498db328a35ba83473ac4714404242f0d242
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 03:15:34 +0700

    Added GetPaymentByIdQuery.cs and GetPaymentByIdQueryHandler.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Payments/Queries/GetPaymentById/GetPaymentByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Payments.Queries.GetPaymentById
+{
+    public class GetPaymentByIdQuery : IRequest<PaymentDto>
+    {
+        public Guid PaymentId { get; set; }
+    }
+}
---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Payments/Queries/GetPaymentByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MultiTenantInvoice.Application.Common.Interfaces;
+using MultiTenantInvoice.Application.Features.Payments.Queries.GetPaymentById;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Payments.Queries
+{
+    public class GetPaymentByIdQueryHandler : IRequestHandler<GetPaymentByIdQuery, PaymentDto>
+    {
+        private readonly IAppDbContext _dbContext;
+
+        public GetPaymentByIdQueryHandler(IAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<PaymentDto> Handle(GetPaymentByIdQuery request,CancellationToken cancellationToken)
+        {
+            var payment = await _dbContext.Payments
+                .FirstAsync(x => x.Id == request.PaymentId, cancellationToken);
+
+            return new PaymentDto
+            {
+                Id = payment.Id,
+                InvoiceId = payment.InvoiceId,
+                Amount = payment.Amount,
+                Method = payment.Method.ToString(),
+                Status = payment.Status.ToString()
+            };
+        }
+    }
+}
