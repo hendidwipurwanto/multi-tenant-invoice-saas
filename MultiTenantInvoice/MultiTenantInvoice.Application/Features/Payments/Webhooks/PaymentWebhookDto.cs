8c11b0bf3dbca5014f5b681860d3afbde352d2ee
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 02:20:15 +0700

    dded wePaymentWebhookDto

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Payments/Webhooks/PaymentWebhookDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Payments.Webhooks
+{
+    public class PaymentWebhookDto
+    {
+        public string EventType { get; set; }
+
+        public Guid PaymentId { get; set; }
+
+        public Guid InvoiceId { get; set; }
+    }
+}
