5f658d172552b43d7f2d6163ba74f8818fc82029
hendi.dp <hendi.dp@pttmg.com>
2026-03-26 10:18:35 +0700

    Added InvoiceCreatedEvent.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Events/InvoiceCreatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Events
+{
+    public class InvoiceCreatedEvent : IDomainEvent
+    {
+        public Guid InvoiceId { get; }
+
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+
+        public InvoiceCreatedEvent(Guid invoiceId)
+        {
+            InvoiceId = invoiceId;
+        }
+    }
+}
