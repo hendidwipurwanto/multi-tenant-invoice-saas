0380809b69f7968d1ce369cb37e43365662eb841
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-26 14:54:35 +0700

    Added new properties into AuditLog table

--- MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Migrations/20260326075346_Added_new_properties_of_auditlog.Designer.cs
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Migrations/20260326075346_Added_new_properties_of_auditlog.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MultiTenantInvoice.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using MultiTenantInvoice.Infrastructure.Persistence;
 namespace MultiTenantInvoice.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260326075346_Added_new_properties_of_auditlog")]
+    partial class Added_new_properties_of_auditlog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Migrations/20260326075346_Added_new_properties_of_auditlog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiTenantInvoice.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_new_properties_of_auditlog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AuditLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AuditLogs");
+        }
+    }
+}
