30a9fc402201355d9a21d3286ae7dc6dbdb490d1
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 03:14:51 +0700

    Added PaymentDto.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Payments/Queries/GetPaymentById/PaymentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Payments.Queries.GetPaymentById
+{
+    public class PaymentDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid InvoiceId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Method { get; set; }
+
+        public string Status { get; set; }
+    }
+}
