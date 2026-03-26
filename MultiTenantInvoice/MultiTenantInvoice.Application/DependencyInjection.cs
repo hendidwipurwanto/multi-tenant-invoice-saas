975f2890079cbc7fc3f0f6bb2f602f2104dd691a
hendi.dp <hendi.dp@pttmg.com>
2026-03-26 09:34:33 +0700

    Added FluentValidation, FluentValidation.DependencyInjectionextension and update it on DependencyInjection.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MultiTenantInvoice.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+     cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
