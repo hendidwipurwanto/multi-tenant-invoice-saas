4ee7b7903f2b77cb52077a02adecfe892fd96a2b
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 00:54:07 +0700

    Added PaymentAttemptedEvent.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Domain/Events/PaymentAttemptedEvent.cs
@@ -0,0 +1,24 @@
+﻿using MultiTenantInvoice.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Domain.Events
+{
+    public class PaymentAttemptedEvent : IDomainEvent
+    {
+        public Guid PaymentId { get; }
+
+        public Guid InvoiceId { get; }
+
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+
+        public PaymentAttemptedEvent(Guid paymentId, Guid invoiceId)
+        {
+            PaymentId = paymentId;
+            InvoiceId = invoiceId;
+        }
+    }
+}
