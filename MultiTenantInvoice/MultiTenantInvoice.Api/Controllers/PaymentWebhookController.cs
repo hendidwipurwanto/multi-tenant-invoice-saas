8d0ae22d79e3dc4fceb2832b85e9948ba217aa8f
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 02:56:42 +0700

    Added PaymentWebhookController.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Api/Controllers/PaymentWebhookController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantInvoice.Application.Features.Payments.Webhooks;
+
+namespace MultiTenantInvoice.Api.Controllers
+{
+    [ApiController]
+    [Route("api/webhooks/payments")]
+    public class PaymentWebhookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentWebhookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HandleWebhook(
+            [FromBody] PaymentWebhookDto dto)
+        {
+            var command = new HandlePaymentWebhookCommand
+            {
+                EventType = dto.EventType,
+                PaymentId = dto.PaymentId,
+                InvoiceId = dto.InvoiceId
+            };
+
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
