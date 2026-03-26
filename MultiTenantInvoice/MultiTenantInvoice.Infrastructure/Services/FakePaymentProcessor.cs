7e2c6b2ee643b3f86a5e820fc2c116db8907c35e
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-26 17:28:40 +0700

    Added FakePaymentProcessor.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Infrastructure/Services/FakePaymentProcessor.cs
@@ -0,0 +1,21 @@
+﻿using MultiTenantInvoice.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Infrastructure.Services
+{
+    public class FakePaymentProcessor : IPaymentProcessor
+    {
+        public Task<bool> ProcessPayment(decimal amount)
+        {
+            var random = new Random();
+
+            var success = random.Next(1, 100) <= 70;
+
+            return Task.FromResult(success);
+        }
+    }
+}
