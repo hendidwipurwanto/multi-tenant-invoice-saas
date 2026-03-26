e41d51ab9f7dbe3e909e999e40d867a6510b659f
hendi.dp <hendi.dp@pttmg.com>
2026-03-26 09:15:37 +0700

    Added CreateInvoiceValidator

---
+++ MultiTenantInvoice/MultiTenantInvoice.Application/Features/Invoices/Validators/CreateInvoiceValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MultiTenantInvoice.Application.Features.Invoices.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantInvoice.Application.Features.Invoices.Validators
+{
+    public class CreateInvoiceValidator : AbstractValidator<CreateInvoiceRequest>
+    {
+        public CreateInvoiceValidator()
+        {
+            RuleFor(x => x.CustomerId)
+                .NotEmpty();
+
+            RuleFor(x => x.Items)
+                .NotEmpty();
+
+            RuleForEach(x => x.Items).ChildRules(item =>
+            {
+                item.RuleFor(x => x.Description)
+                    .NotEmpty()
+                    .MaximumLength(200);
+
+                item.RuleFor(x => x.Quantity)
+                    .GreaterThan(0);
+
+                item.RuleFor(x => x.UnitPrice)
+                    .GreaterThan(0);
+            });
+
+            RuleFor(x => x.DueDate)
+                .GreaterThan(x => x.IssueDate);
+        }
+    }
+}
