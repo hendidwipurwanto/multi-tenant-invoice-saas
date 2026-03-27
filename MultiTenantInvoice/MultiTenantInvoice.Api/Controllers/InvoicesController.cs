479623fc79b0db12e793739ff6d16740eff4c310
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-27 11:36:56 +0700

    Added InvoicesController.cs

---
+++ MultiTenantInvoice/MultiTenantInvoice.Api/Controllers/InvoicesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantInvoice.Application.Features.Invoices.Queries.GetInvoiceTimeline;
+
+namespace MultiTenantInvoice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public InvoicesController(IMediator mediator    )
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}/timeline")]
+        public async Task<IActionResult> GetTimeline(Guid id)
+        {
+            var result = await _mediator.Send(
+                new GetInvoiceTimelineQuery { InvoiceId = id });
+
+            return Ok(result);
+        }
+    }
+}
